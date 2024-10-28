b9d285dba25d32d9e7e00329e5e364d52dad3ff5
Ayke van Laethem <aykevanlaethem@gmail.com>
2024-10-28 10:55:37 +0100

    internal/task: add cooperative implementation of Futex
    See the code comments for details. But in short, this implements a futex
    for the cooperative scheduler (that is single threaded and
    non-reentrant). Similar implementations can be made for basically every
    other operating system, and even WebAssembly with the threading
    (actually: atomics) proposal.
    Using this basic futex implementation means we can use the same
    implementation for synchronisation primitives on cooperative and
    multicore systems.
    
    For more information on futex across operating systems:
    https://outerproduct.net/futex-dictionary.html

--- src/internal/task/atomic-cooperative.go
+++ src/internal/task/atomic-cooperative.go
@@ -5,7 +5,7 @@ package task
 // different goroutine or interrupt happening at the same time.
 
 type atomicIntegerType interface {
-	uintptr | uint64
+	uintptr | uint32 | uint64
 }
 
 type pseudoAtomic[T atomicIntegerType] struct {
@@ -31,6 +31,10 @@ func (x *pseudoAtomic[T]) Swap(new T) (old T) {
 // Uintptr is an atomic uintptr when multithreading is enabled, and a plain old
 // uintptr otherwise.
 type Uintptr = pseudoAtomic[uintptr]
+
+// Uint32 is an atomic uint32 when multithreading is enabled, and a plain old
+// uint32 otherwise.
+type Uint32 = pseudoAtomic[uint32]
 
 // Uint64 is an atomic uint64 when multithreading is enabled, and a plain old
 // uint64 otherwise.
